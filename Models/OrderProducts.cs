fd82e81f87eef7176d5fe88f0f5d5323653410ca
leny133 <63651131+leny133@users.noreply.github.com>
2021-01-04 18:28:19 -0400

    Add project files.

---
+++ Models/OrderProducts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace salon_essentials.Models
+{
+    public class OrderProducts
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public double UnitPrice { get; set; }
+        public double Quantity { get; set; }
+
+        public Product Product { get; set; }
+        public Order Order { get; set; }
+
+        public OrderProducts(DataRow row)
+        {
+            this.OrderId = Convert.ToInt32(row["OrderId"]);
+            this.ProductId = Convert.ToInt32(row["ProductId"]);
+            this.UnitPrice = Convert.ToDouble(row["UnitPrice"]);
+            this.Quantity = Convert.ToDouble(row["Quantity"]);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("salon_essentials")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("salon_essentials")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cf98e98c-c537-4647-aea0-ef78a0837af6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
